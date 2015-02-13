5c1f5896cecb56fab9002f5c89835602f2df578f
jakplugg <code@neutron-sound.com>
2015-02-13 11:37:51 -0500

    interpolation test
    the interpolation beta 1 added
    note, tuning will be different, not fixed because ISR rate is not final.

---
+++ orgone_accumulator/official_firmware/orgone_accumulator_interpolation_beta/config.h
@@ -0,0 +1,15 @@
+//neutron orgone accumulator config file. you can save this between firmware updates. 
+
+#define TUNEMULT 59000  //this is the base frequency. halving it will lower the entire oscillator by 1 octave 
+
+#define NOTESIZE 72.0  // note resolution. lower value will give wider oscillator range, higher will give better resolution
+//you will need to recalibrate the v/oct trimmer if you adjust this. decimal is important, even is zero.
+
+#define LED_COMP 1000  //LED compensation. adjusts how the LEDs fade across positions
+
+#define TUNE_LOCK_PROCESS 1 //0 = normal tune lock 1 = extra modes. //dont change in this beta
+
+
+
+
+ 
